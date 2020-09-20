131f4557851eac871ca5e471e3b9eb8b667445a6
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-09-20 13:33:15 +0100

    Set client id for debugging proposes.

--- app.go
+++ app.go
@@ -56,6 +56,7 @@ func main() {
 	opts.SetPingTimeout(10 * time.Second)
 	opts.OnConnect = subscribeToCloudBuildTopic
 	opts.SetCleanSession(true)
+	opts.SetClientID("google-cloud-build-status")
 
 	println("Connecting to: ", mqtt_url)
 	c := mqtt.NewClient(opts)
